4d9f5333c73504fe03d3c7fac07fbddb6b3c78f3
sakilk130 <sakilk130@gmail.com>
2021-10-29 22:02:24 +0600

    complete sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="app">
-      {/* 1. Header */}
       <Header />
-      {/* 2. App body */}
-      {/* 2.1 Sidebar */}
-      {/* 2.2 Feed */}
-      {/* 2.3 Widgets */}
+      <div className="app__body">
+        <Sidebar />
+        {/* 2.2 Feed */}
+        {/* 2.3 Widgets */}
+      </div>
     </div>
   );
 }
