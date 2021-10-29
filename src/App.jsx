4f5f86895bbd9ce9c52d3283a2de1b60d36f8e54
sakilk130 <sakilk130@gmail.com>
2021-10-29 21:43:37 +0600

    complete header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>React</h1>
+      {/* 1. Header */}
+      <Header />
+      {/* 2. App body */}
+      {/* 2.1 Sidebar */}
+      {/* 2.2 Feed */}
+      {/* 2.3 Widgets */}
     </div>
   );
 }
